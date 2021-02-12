74428b35e67d8c9da2e1acab945707318e9ce687
pkslty <pkslty@yandex.ru>
2021-02-12 13:58:20 +0300

    функция определения четного числа

--- L2_DenisKuzmin/L2_DenisKuzmin/main.swift
+++ L2_DenisKuzmin/L2_DenisKuzmin/main.swift
@@ -7,8 +7,6 @@
 
 import Foundation
 
-print("Hello, World!")
-
 func fibonacci(num: Int) -> Int {
     let sign: Int = num >= 0 ? 1 : -1
     if (num == 0) {
@@ -19,4 +17,32 @@ func fibonacci(num: Int) -> Int {
     return fibonacci(num: num - sign) + fibonacci(num: num - sign * 2)
 }
 
-print(fibonacci(num: 50))
+func fibonacciArray(num: Int) -> [Int] {
+    var arr = [Int]()
+    let sign: Int = num >= 0 ? 1 : -1
+    arr.append(0)
+    if (num == 0) {
+        return arr
+    }
+    arr.append(sign)
+    if (abs(num) == 1)  {
+        return arr
+    }
+    for i in (2...abs(num)) {
+        arr.append(arr[i-1] + arr[i-2])
+    }
+    
+    return arr
+}
+
+print(fibonacci(num: 30))
+print(fibonacciArray(num: -50))
+
+func isOdd(_ num: Int) -> Bool {
+    if (num % 2 == 0) {
+        return true
+    }
+    return false
+}
+print(isOdd(0), isOdd(1), isOdd(2), isOdd(47), isOdd(-1), isOdd(-9), isOdd(5))
+
